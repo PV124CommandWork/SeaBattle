4c33749a0dcbf7d84094275d78b511cfeee5196a
Toha229 <kramunuchka5402@gmail.com>
2022-10-26 20:49:48 +0300

    Added Batlefield

--- SeaBattle/GameDBContext/GameDBContext.cs
+++ SeaBattle/GameDBContext/GameDBContext.cs
@@ -14,7 +14,7 @@ namespace GameDBContext
         public DbSet<CurrentBattles> CurrentBattles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=HELLOPC;Initial Catalog=GameDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GameDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
