ca71d7cebe80257b9d0728396d45fdc485071cae
senivan <senivan1997@gmail.com>
2022-11-10 15:07:54 +0200

    Added completeBattle function

--- SeaBattle/SeaBattleServer/BattleManagement.cs
+++ SeaBattle/SeaBattleServer/BattleManagement.cs
@@ -50,6 +50,28 @@ namespace SeaBattleServer
                 DataBaseAccess.DbContext.SaveChanges();
             }
         }
+        public static void completeBattle(string winnerLogin)
+        {
+            CurrentBattle battle = (from b in DataBaseAccess.DbContext.CurrentBattles where b.Users[0].Login == winnerLogin || b.Users[1].Login == winnerLogin select b).FirstOrDefault();
+            if(battle == null)
+            {
+                throw new Exception();
+            }
+            User winner = (from u in battle.Users where u.Login == winnerLogin select u).FirstOrDefault();
+            User loser = (from u in battle.Users where u.Login != winnerLogin select u).FirstOrDefault();
+            if (winner == null || loser == null) {
+                throw new Exception();
+            }
+            winner.Reward.BattlesPlayed++;
+            loser.Reward.BattlesPlayed++;
+            winner.Reward.BattlesWon++;
+            deleteBattle(battle.Id);
+            DataBaseAccess.DbContext.Rewards.Update(winner.Reward);
+            DataBaseAccess.DbContext.Rewards.Update(loser.Reward);
+            DataBaseAccess.DbContext.Users.Update(winner);
+            DataBaseAccess.DbContext.Users.Update(loser);
+            DataBaseAccess.DbContext.SaveChanges();
+        }
     }
 }
  
